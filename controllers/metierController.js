f0da2e6957ec6f1f5f1b4e20cabf71bbb881cdf6
Amine B <amine-mourad.bencharif8@etu.univ-lorraine.fr>
2019-01-25 22:44:12 +0100

    http://localhost:3000/metier form
    added semi-functionnal /metier form withou db submitting yet

--- controllers/metierController.js
+++ controllers/metierController.js
@@ -2,7 +2,13 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('sample text');
+    res.render("metier/addOrEdit", {
+        viewTitle : "Insérer Metier"
+    });
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body)
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const path = require('path');
 const exphdlbs = require('express-handlebars');
 
 const metierController = require('./controllers/metierController');
+const bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+app.use(bodyParser.json());
 app.set('views', path.join(__dirname, './views'));
-app.engine('hbs', exphdlbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutDir: __dirname + '/views/layouts/'}))
+app.engine('hbs', exphdlbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'hbs');
 app.listen(3000, () =>  {
     console.log('Server express a demarré sur le port : 3000');
