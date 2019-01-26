476007ae56ecc238e3d9499f2d30fa32c7f04177
Amine B <amine-mourad.bencharif8@etu.univ-lorraine.fr>
2019-01-26 12:10:57 +0100

    Fully working CRUD
    added Edit & Delete

--- controllers/metierController.js
+++ controllers/metierController.js
@@ -5,12 +5,17 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
     res.render("metier/addOrEdit", {
-        viewTitle : "Insérer Metier"
+        viewTitle : "Insérer métier"
     });
 });
 
 router.post('/', (req, res) => {
-    insertRecord(req, res);
+    if (req.body._id == '') {
+        insertRecord(req, res);
+    }else {
+        updateRecord(req, res);
+    }
+
 });
 
 function insertRecord(req, res) {
@@ -26,7 +31,7 @@ function insertRecord(req, res) {
             if (err.name == 'ValidationError') {
                 handleValidationError(err, req.body);
                 res.render("metier/addOrEdit", {
-                    viewTitle : "Insérer Metier",
+                    viewTitle : "Insérer métier",
                     metier : req.body
                 })
             } else {
@@ -49,6 +54,34 @@ router.get('/list', (req, res) => {
     })
 });
 
+router.get('/delete/:id', (req, res) => {
+    Metier.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err){
+            res.redirect('/metier/list');
+        }else {
+            console.log('Erreur lors de la suppression :' + err);
+        }
+    });
+});
+
+function updateRecord(req, res) {
+    Metier.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) {
+            res.redirect('metier/list');
+        }else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("metier/addOrEdit", {
+                    viewTitle: 'Mis a jour Metier',
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Impossible de mettre à jour : ' + err);
+        }
+    });
+}
+
 function handleValidationError(err, body) {
     for (field in err.errors) {
         switch (err.errors[field].path) {
@@ -64,5 +97,16 @@ function handleValidationError(err, body) {
         }
     }
 }
+router.get('/:id', (req, res) => {
+    Metier.findById((req.params.id), (err, docs) => {
+        if (!err) {
+            res.render("metier/addOrEdit", {
+                viewTitle: "Mettre à jour métier",
+                metier: docs
+            });
+        }
+    });
+});
+
 
 module.exports = router;
